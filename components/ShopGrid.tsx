a0ae8744fb1d0363dd758168d8dd5db60939c200
Joliver-crypto <justin.oliver.72@gmail.com>
2025-08-28 09:50:39 -0700

    Update4 Mobile Bugs

--- components/ShopGrid.tsx
+++ components/ShopGrid.tsx
@@ -21,7 +21,7 @@ export default function ShopGrid({ works }: ShopGridProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8 ">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8">
       {availableWorks.map((work) => (
         <div
           key={work.slug}
